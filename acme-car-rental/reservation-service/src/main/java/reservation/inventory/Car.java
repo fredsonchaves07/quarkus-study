d05f0f9b14042ca5d355cd43b92a67b514963b16
fredsonchaves07 <fredsonchaves07@gmail.com>
2023-08-09 10:57:42 -0300

    feat: initial config project acme rental service

---
+++ acme-car-rental/reservation-service/src/main/java/reservation/inventory/Car.java
@@ -0,0 +1,35 @@
+package reservation.inventory;
+
+public class Car {
+
+    private Long id;
+
+    private String licensePlateNumber;
+
+    private String manufacturer;
+
+    private String model;
+
+    public Car(Long id, String licensePlateNumber, String manufacturer, String model) {
+        this.id = id;
+        this.licensePlateNumber = licensePlateNumber;
+        this.manufacturer = manufacturer;
+        this.model = model;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLicensePlateNumber() {
+        return licensePlateNumber;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+}
